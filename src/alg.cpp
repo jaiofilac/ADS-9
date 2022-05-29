b2c8d59d54317b73ecd62db4e8affe641e9dda1b
Андрей Жаворонков <96244220+jaiofilac@users.noreply.github.com>
2022-05-29 21:29:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,31 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> tree;
+    std::fstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return tree;
+    }
+    std::string tmp;
+    while (!file.eof()) {
+        char ch = file.get();
+        if (ch >= 'a' && ch <= 'z') {
+            tmp += ch;
+        }
+        else if (ch >= 'A' && ch <= 'Z') {
+            tmp += std::tolower(ch);
+        }
+        else {
+            if (tmp != "") {
+                tree.add(tmp);
+            }
+            tmp.clear();
+        }
+    }
+    if (tmp != "") {
+        tree.add(tmp);
+    }
+    file.close();
+    return tree;
 }
